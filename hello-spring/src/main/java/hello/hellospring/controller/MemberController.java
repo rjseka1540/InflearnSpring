364fc54025f1cffa1f54bbf8e8bcedc117949949
rjseka1540 <rjseka1540@naver.com>
2023-03-20 23:32:23 +0900

    20230320 Spring Bean과 의존 관계(DI) 이해  - 컴포넌트 스캔  - 자바 코드로 직접 등록

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+//컴포넌트 스캔을 활용한 스프링 빈
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,18 +2,36 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     //Dependency Injection(DI : 의존관계 주입)
+    //DI 필드 주입식 : 비권장
+    //@Autowired private MemberRepository memberRepository;
+
+    //DI Setter 주입식 : 아무나 코드 수정 가능 -> 비권장
+    /**
+     *  private MemberRepository memberRepository;
+     *     @Autowired
+     *     public void setMemberRepository(MemberRepository memberRepository) {
+     *         this.memberRepository = memberRepository;
+     *     }
+     */
+   
+    //DI 생성자 주입식 : 요즘 씀
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+    //Dependency Injection(DI : 의존관계 주입)
 
     /**
      * 회원가입
